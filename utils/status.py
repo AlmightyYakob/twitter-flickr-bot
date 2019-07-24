cad5a607b95dc932e5e6650685841e7dac7ed419
Jacob Nesbitt <nesbitjj@clarkson.edu>
2019-07-24 00:02:06 -0400

    Add conditional capitalization of title

--- utils/status.py
+++ utils/status.py
@@ -1,10 +1,12 @@
 import json
 
 def createStatusFromPhoto(photo):
-  print(json.dumps(photo, indent=4))
   photo = photo['photo']
   url = photo['urls']['url'][0]['_content']
   hashtags = list(map(lambda x: f"#{x['_content']}", photo['tags']['tag']))
   title = photo['title']['_content']
+
+  if (not title[0].isupper()):
+    title = title.capitalize()
 
   return f"{title}\n{' '.join(hashtags)}\n{url}"
