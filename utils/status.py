d6382c413d2b9a0c3412e9b364092088d2983894
Jacob Nesbitt <nesbitjj@clarkson.edu>
2019-07-24 00:05:02 -0400

    Add description to status

--- utils/status.py
+++ utils/status.py
@@ -5,8 +5,9 @@ def createStatusFromPhoto(photo):
   url = photo['urls']['url'][0]['_content']
   hashtags = list(map(lambda x: f"#{x['_content']}", photo['tags']['tag']))
   title = photo['title']['_content']
+  description = photo['description']['_content']
 
   if (not title[0].isupper()):
     title = title.capitalize()
 
-  return f"{title}\n{' '.join(hashtags)}\n{url}"
+  return f"{title}\n{description}\n{' '.join(hashtags)}\n{url}"
